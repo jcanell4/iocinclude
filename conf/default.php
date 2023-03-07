fa1a9ebac1a10c0ede1e93150b09dfb2cd139333
marjosepg <marjose@ioc.cat>
2023-03-07 14:04:35 +0100

    Per defecte no es mostra subapartats

--- conf/default.php
+++ conf/default.php
@@ -5,7 +5,7 @@
 $conf['noheader']      = 0;      // Don't display the header of the inserted section
 $conf['firstseconly']  = 0;      // limit entries on main blog page to first section
 $conf['showtaglogos']  = 0;      // display image for first tag
-$conf['showfooter']    = 1;      // display meta line below blog entries
+$conf['showfooter']    = 0;      // display meta line below blog entries
 $conf['showlink']      = 0;      // link headlines of blog entries
 $conf['showpermalink'] = 0;      // show permalink below blog entries
 $conf['showdate']      = 1;      // show date below blog entries
@@ -25,6 +25,6 @@ $conf['safeindex']    = 1;       // prevent indexing of protected metadata
 $conf['order']        = 'id';    // order in which the pages are included in the case of multiple pages
 $conf['rsort']        = 0;       // reverse sort order
 $conf['depth']        = 1;       // maximum depth of namespace includes, 0 for unlimited depth
-$conf['readmore']     = 1;       // Show readmore link in case of firstsection only
+$conf['readmore']     = 0;       // Show readmore link in case of firstsection only
 $conf['debugoutput']  = 0;       // print debug information to debuglog if global allowdebug is enabled
 //Setup VIM: ex: et ts=2 :
